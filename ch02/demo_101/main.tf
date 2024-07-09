0850f6799b1272f42b8de8fa53e6b74afb2978a2
fontanads <fontanads@gmail.com>
2024-07-09 17:33:11 +0100

    ec2 webserver toy example

--- ch02/demo_101/main.tf
+++ ch02/demo_101/main.tf
@@ -5,9 +5,29 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0887c7673564b061c"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.my_instance_sec_group.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    user_data_replace_on_change = true  # because user_data typically runs only once during first boot
+
 
     tags = {
         Name = "terraform-example-ch02-101"
     }
 }
 
+resource "aws_security_group" "my_instance_sec_group" {
+    name = "terraform-example-ch02-101"
+    
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
